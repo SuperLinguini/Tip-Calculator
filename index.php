03f2414ab23da23a439f945be479a8f7400c0e8d
Milind Lingineni <milind.lingineni@gmail.com>
2016-12-18 16:01:15 -0500

    Added PHP tip calculations

--- index.php
+++ index.php
@@ -2,21 +2,27 @@
 <body>
     <h2>Tip Calculator</h2>
     <?php
-        if(isset($_GET['subtotal']))
+        if(isset($_GET['subtotal']) && ((float)$_GET['subtotal']) > 0)
         {
-            $total = (float)$_GET["subtotal"] * (float)$_GET['tip'];
-            $text = "Subtotal is $total dollars. <br>";
-            echo $text;
+            $tip = (float)$_GET["subtotal"] * (float)$_GET['tip'];
+            $total = $tip + (float)$_GET["subtotal"];
         }
     ?>
     <form action="index.php" method="GET">
         Bill subtotal: $
-        <input type="text" name="subtotal" value="<?php echo $total?>"><br>
+        <input type="text" name="subtotal" value="<?php if(isset($_GET['subtotal'])) echo $_GET['subtotal']?>"><br>
         Tip percentage:
         <input type="radio" name="tip" value=".10" checked> 10%
         <input type="radio" name="tip" value=".15"> 15%
-        <input type="radio" name="tip" value=".20"> 20%
+        <input type="radio" name="tip" value=".20"> 20% <br>
         <input type="submit" value="Submit">
     </form>
+    <?php
+        if(isset($tip) && isset($total) && is_float($tip) && is_float($total))
+        { 
+            echo "Tip: $tip<br>";
+            echo "Total: $total dollars. <br>";
+        }
+    ?>
 </body>
 </html>
